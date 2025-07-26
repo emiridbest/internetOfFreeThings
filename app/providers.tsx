5ea91af13d030efe04193352768a826bc22a5dee
emiridbest <udogwuemiri@gmail.com>
2025-07-26 13:55:02 +0100

    refactor: add client ID

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,7 +10,8 @@ export default function AppProvider({ children }: { children: ReactNode }) {
 
   return (
       <PrivyProvider
-        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}    
+        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+        clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}    
         config={{
           loginMethods: ['email', 'wallet'],
           appearance: {
